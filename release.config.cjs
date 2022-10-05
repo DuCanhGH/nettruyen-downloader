bc8bd8c6f69331e026c51e71aea79810c41957c3
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-10-05 23:28:03 +0700

    feat: test

---
+++ release.config.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("semantic-release").Options}
+ */
+module.exports = {
+  branches: [
+    { name: "master" },
+    { name: "pre/rc", channel: "pre/rc", prerelease: "rc" },
+    { name: "beta", channel: "beta", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+  ],
+};
