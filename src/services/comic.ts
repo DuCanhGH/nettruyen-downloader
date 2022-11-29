e6a92782e98d728acd5f3c18b87dc00caa2d7d39
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2022-11-29 18:18:03 +0700

    fix: remove try-catch in services/comic

--- src/services/comic.ts
+++ src/services/comic.ts
@@ -10,46 +10,38 @@ export interface ComicInfo {
 }
 
 export const getComicInfo = async (comicURL: string): Promise<ComicInfo> => {
-  try {
-    puppeteer.use(StealthPlugin());
-    const browser = await puppeteer.launch({
-      executablePath: executablePath(),
-    });
-    const page = await browser.newPage();
-    await page.goto(comicURL);
-    const title = await page.evaluate(
-      () => document.querySelector("#item-detail .title-detail")?.textContent
-    );
-    if (!title) {
-      throw new Error("404");
-    }
-    const chapters = await page.evaluate(() =>
-      Array.from(document.querySelectorAll(".list-chapter ul li:not(.heading)"))
-        .map((li) => {
-          const title = li.querySelector(".chapter a")?.textContent;
-          const url = li.querySelector(".chapter a")?.getAttribute("href");
-          if (!title || !url) {
-            throw new Error("404");
-          }
-          return {
-            title,
-            url,
-            images: [] as ImageType[],
-          };
-        })
-        .reverse()
-    );
-    const result = {
-      title,
-      chapters,
-    };
-    await browser.close();
-    return result;
-  } catch (err) {
-    console.log(err);
-    return {
-      title: "",
-      chapters: [],
-    };
+  puppeteer.use(StealthPlugin());
+  const browser = await puppeteer.launch({
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+  await page.goto(comicURL);
+  const title = await page.evaluate(
+    () => document.querySelector("#item-detail .title-detail")?.textContent
+  );
+  if (!title) {
+    throw new Error("404");
   }
+  const chapters = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".list-chapter ul li:not(.heading)"))
+      .map((li) => {
+        const title = li.querySelector(".chapter a")?.textContent;
+        const url = li.querySelector(".chapter a")?.getAttribute("href");
+        if (!title || !url) {
+          throw new Error("404");
+        }
+        return {
+          title,
+          url,
+          images: [] as ImageType[],
+        };
+      })
+      .reverse()
+  );
+  const result = {
+    title,
+    chapters,
+  };
+  await browser.close();
+  return result;
 };
